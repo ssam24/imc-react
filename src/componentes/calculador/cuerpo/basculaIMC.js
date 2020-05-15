1b018116fc09027510ae7df0ac57ca4f9b7669f8
samuel crespo <crespoalzatesamuel@gmail.com>
2020-05-15 13:03:06 -0500

    realizo cambios y agrego imagen de bascula

---
+++ src/componentes/calculador/cuerpo/basculaIMC.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import img_bascula from '../../../../public/bascula-dibujo.png';
+function bs(){
+    return (
+        <article>
+
+        </article>
+    );
+}
+
+export default bs; 
